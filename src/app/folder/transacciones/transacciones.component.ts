4c6fd802c939b7d9aca873808101840c9b7d97c9
arios.bssltda1 <arios.bssltda@gmail.com>
2023-07-27 20:24:38 -0500

    transactions

--- src/app/folder/transacciones/transacciones.component.ts
+++ src/app/folder/transacciones/transacciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
 
@@ -12,6 +13,12 @@ export class TransaccionesComponent  implements OnInit {
   @ViewChild(IonModal) modal!: IonModal;
   message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
   name: string = "";
+
+  documentType: FormControl = new FormControl("", Validators.required);
+  documentNumber: FormControl = new FormControl("", Validators.required);
+  value: FormControl = new FormControl(null, Validators.required);
+  donation: FormControl = new FormControl(false, Validators.required);
+
   constructor() { }
 
   ngOnInit() {}
@@ -31,29 +38,37 @@ export class TransaccionesComponent  implements OnInit {
     }
   }
 
-  public data = [
-    'Nicole',
-    'Alejandro',
-    'Sebastian',
-    'Luisa',
-    'Sofía',
-    'Diego',
-    'Laura',
-    'Luis Ángel',
-    'Germán',
-    'Sofía',
-    'Luna',
-    'Arturo',
-    'Viviana',
-    'Wilson',
-    'Zaira'
+  public peopleData = [
+    {name: 'Nicole', document: 10, type: "CC"},
+    {name: 'Alejandro', document: 10, type: "CC"},
+    {name: 'Sebastian', document: 10, type: "CC"},
+    {name: 'Luisa', document: 10, type: "CC"},
+    {name: 'Sofía', document: 10, type: "CC"},
+    {name: 'Diego', document: 10, type: "CC"},
+    {name: 'Laura', document: 10, type: "CC"},
+    {name: 'Luis Ángel', document: 10, type: "CC"},
+    {name: 'Germán', document: 10, type: "CC"},
+    {name: 'Sofía', document: 10, type: "CC"},
+    {name: 'Luna', document: 10, type: "CC"},
+    {name: 'Arturo', document: 10, type: "CC"},
+    {name: 'Viviana', document: 10, type: "CC"},
+    {name: 'Wilson', document: 10, type: "CC"},
+    {name: 'Zaira', document: 10, type: "CC"}
   ];
 
-  public results = [...this.data];
+  public resultPeople: any = this.peopleData;
 
   handleInput(event: any) {
-    const query = event.target.value.toLowerCase();
-    this.results = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1);
+    const nameQuery = event.target.value.toLowerCase();
+    this.resultPeople = this.peopleData.filter((d) => d.name.toLowerCase().indexOf(nameQuery) > -1);
+  }
+
+  selectName() {
+
+  }
+
+  donationState(event: Event) {
+    this.donation.setValue((event as CustomEvent).detail.checked);
   }
 
 }
