c8e5af2c1c818b25093263a28823ef0c0acb8bbe
Alejandro <luisrios.lar@gmail.com>
2025-05-03 12:07:59 -0500

    style: login button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,115 +5,98 @@ import { StoreService } from './services/store.service';
 import { Platform } from '@ionic/angular';
 import { StatusBar, Style } from '@capacitor/status-bar';
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  standalone: false,
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.scss'],
+	standalone: false,
 })
 export class AppComponent {
-  public appPages: any = [];
+	public appPages: any = [];
 
-  currentFolder: string = '';
+	currentFolder: string = '';
 
-  constructor(
-    router: Router,
-    private storeService: StoreService,
-    private platform: Platform
-  ) {
-    this.initializeApp();
-    window.addEventListener('visibilitychange', function () {
-      if (document.visibilityState === 'visible') {
-        console.log('APP resumed');
-        window.location.reload();
-      }
-    });
+	constructor(router: Router, private storeService: StoreService, private platform: Platform) {
+		this.initializeApp();
+		window.addEventListener('visibilitychange', function () {
+			if (document.visibilityState === 'visible') {
+				console.log('APP resumed');
+				window.location.reload();
+			}
+		});
 
-    effect(() => {
-      this.appPages = [];
-      this.appPages.push({
-        title: 'Progreso',
-        url: '/progreso',
-        icon: 'bar-chart',
-      });
-      this.appPages.push({ title: 'Tarifas', url: '/metas', icon: 'ribbon' });
-      this.appPages.push({
-        title: 'Abonos',
-        url: '/transacciones',
-        icon: 'cash',
-      });
-      this.appPages.push({ title: 'Salir', url: '/inicio', icon: 'home' });
+		effect(() => {
+			this.appPages = [];
+			this.appPages.push({
+				title: 'Progreso',
+				url: '/progreso',
+				icon: 'bar-chart',
+			});
+			this.appPages.push({ title: 'Tarifas', url: '/metas', icon: 'ribbon' });
+			this.appPages.push({
+				title: 'Abonos',
+				url: '/transacciones',
+				icon: 'cash',
+			});
+			this.appPages.push({ title: 'Salir', url: '/inicio', icon: 'home' });
 
-      if (
-        (this.storeService.userData()[0]?.ADMIN === 1 ||
-          this.storeService.userData()[0]?.ADMIN === 3) &&
-        this.appPages.find((page: any) => page.title === 'Errores') ===
-          undefined
-      ) {
-        this.appPages.splice(-1, 0, {
-          title: 'Errores',
-          url: '/fallas',
-          icon: 'alert-circle',
-        });
-      }
-      if (
-        this.storeService.userData()[0]?.ADMIN > 0 &&
-        this.appPages.find((page: any) => page.title === 'Listado General') ===
-          undefined
-      ) {
-        this.appPages.splice(-4, 0, {
-          title: 'Listado General',
-          url: '/listado-general',
-          icon: 'accessibility',
-        });
-      }
-      console.log('?> ', this.storeService.userData()[0]);
-      if (
-        (this.storeService.userData()[0]?.LEADER ||
-          this.storeService.userData()[0]?.ADMIN === 3) &&
-        this.appPages.find((page: any) => page.title === 'Área') === undefined
-      ) {
-        console.log('Entra aquí perfectamente');
-        const area: string = `/area/${
-          storeService.userData()[0]?.ADMIN === 1 ||
-          storeService.userData()[0]?.ADMIN === 3
-            ? 'all'
-            : storeService.userData()[0]?.LEADER
-        }`;
-        this.appPages.splice(-1, 0, {
-          title: 'Áreas',
-          url: area,
-          icon: 'people',
-        });
-      }
-      if (
-        this.storeService.userData()[0]?.ADMIN === 3 &&
-        this.appPages.find((page: any) => page.title === 'Reportes') ===
-          undefined
-      ) {
-        this.appPages.splice(-3, 0, {
-          title: 'Descargar Reporte',
-          url: '/panel',
-          icon: 'document',
-        });
-      }
-    });
-    router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event) => {
-        this.currentFolder = (
-          event as NavigationEnd
-        ).urlAfterRedirects.substring(1);
-      });
-    if (localStorage.getItem('userData')) {
-      storeService.userData.set(
-        JSON.parse(localStorage.getItem('userData') || '')
-      );
-    }
-  }
+			if (
+				(this.storeService.userData()[0]?.ADMIN === 1 || this.storeService.userData()[0]?.ADMIN === 3) &&
+				this.appPages.find((page: any) => page.title === 'Errores') === undefined
+			) {
+				this.appPages.splice(-1, 0, {
+					title: 'Errores',
+					url: '/fallas',
+					icon: 'alert-circle',
+				});
+			}
+			if (
+				this.storeService.userData()[0]?.ADMIN > 0 &&
+				this.appPages.find((page: any) => page.title === 'Listado General') === undefined
+			) {
+				this.appPages.splice(-4, 0, {
+					title: 'Listado General',
+					url: '/listado-general',
+					icon: 'accessibility',
+				});
+			}
+			if (
+				(this.storeService.userData()[0]?.LEADER || this.storeService.userData()[0]?.ADMIN === 3) &&
+				this.appPages.find((page: any) => page.title === 'Área') === undefined
+			) {
+				console.log('Entra aquí perfectamente');
+				const area: string = `/area/${
+					storeService.userData()[0]?.ADMIN === 1 || storeService.userData()[0]?.ADMIN === 3
+						? 'all'
+						: storeService.userData()[0]?.LEADER
+				}`;
+				this.appPages.splice(-1, 0, {
+					title: 'Áreas',
+					url: area,
+					icon: 'people',
+				});
+			}
+			if (
+				this.storeService.userData()[0]?.ADMIN === 3 &&
+				this.appPages.find((page: any) => page.title === 'Reportes') === undefined
+			) {
+				this.appPages.splice(-3, 0, {
+					title: 'Descargar Reporte',
+					url: '/panel',
+					icon: 'document',
+				});
+			}
+		});
+		router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
+			this.currentFolder = (event as NavigationEnd).urlAfterRedirects.substring(1);
+		});
+		if (localStorage.getItem('userData')) {
+			storeService.userData.set(JSON.parse(localStorage.getItem('userData') || ''));
+		}
+	}
 
-  async initializeApp() {
-    await this.platform.ready();
-    await StatusBar.setStyle({ style: Style.Light });
-    await StatusBar.setBackgroundColor({ color: '#ffffff' });
-  }
+	async initializeApp() {
+		await this.platform.ready();
+		await StatusBar.setStyle({ style: Style.Light });
+		await StatusBar.setBackgroundColor({ color: '#ffffff' });
+	}
 }
--- src/app/folder/inicio/inicio.page.ts
+++ src/app/folder/inicio/inicio.page.ts
@@ -1,191 +1,164 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
-// import * as dayjs from 'dayjs';
-// import * as dayOfYear from 'dayjs/plugin/dayOfYear';
-// dayjs.extend(dayOfYear);
 
 import { map } from 'rxjs/internal/operators/map';
 import { CrudService } from 'src/app/services/crud.service';
 import { StoreService } from 'src/app/services/store.service';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
-  standalone: false,
+	selector: 'app-inicio',
+	templateUrl: './inicio.page.html',
+	styleUrls: ['./inicio.page.scss'],
+	standalone: false,
 })
 export class InicioPage {
-  alertMessage: string = '';
-  document = new FormControl('', Validators.required);
-  type = new FormControl('', Validators.required);
-  password = new FormControl('');
-  enableButton: boolean = true;
-  showLoadingText: boolean = false;
-  maintenance: boolean | null = null;
-  enableAdmin: boolean = false;
-  saveAdminData: any;
-  countDown: any;
-  interval: any;
+	alertMessage: string = '';
+	document = new FormControl('', Validators.required);
+	type = new FormControl('', Validators.required);
+	password = new FormControl('');
+	enableButton: boolean = true;
+	showLoadingText: boolean = false;
+	maintenance: boolean | null = null;
+	enableAdmin: boolean = false;
+	saveAdminData: any;
 
-  innheight: number = 0;
-  outheight: number = 0;
-  clientHeight: number = 0;
+	innheight: number = 0;
+	outheight: number = 0;
+	clientHeight: number = 0;
 
-  @ViewChild('errorToast') errorToast!: HTMLIonToastElement;
+	@ViewChild('errorToast') errorToast!: HTMLIonToastElement;
 
-  constructor(
-    private crudService: CrudService,
-    private storeService: StoreService,
-    private router: NavController
-  ) {
-    this.innheight = window.innerHeight;
-    this.outheight = window.outerHeight;
-    this.clientHeight = window.document.documentElement.clientHeight;
-    document
-      .getElementsByClassName('redbar')
-      .item(0)
-      ?.setAttribute(
-        'style',
-        'bottom: ' + innerHeight + this.outheight + this.clientHeight + 'px;'
-      );
-  }
+	constructor(private crudService: CrudService, private storeService: StoreService, private router: NavController) {
+		this.innheight = window.innerHeight;
+		this.outheight = window.outerHeight;
+		this.clientHeight = window.document.documentElement.clientHeight;
+		document
+			.getElementsByClassName('redbar')
+			.item(0)
+			?.setAttribute('style', 'bottom: ' + innerHeight + this.outheight + this.clientHeight + 'px;');
+	}
 
-  ionViewDidEnter() {
-    this.updateTime();
-    localStorage.removeItem('userData');
-    this.storeService.userData.set([]);
-    this.crudService
-      .checkMaintenance()
-      .pipe(
-        map((res) => {
-          return res[0].VALUE === '1' ? true : false;
-        })
-      )
-      .subscribe({
-        next: (res) => {
-          this.maintenance = res;
-        },
-        error: () => {
-          this.alertMessage = 'Ocurrió un error inesperado, reintentando';
-          this.errorToast.present();
-          console.error(
-            'Ocurrió un error al intentar verificar si la aplicación está en mantenimiento'
-          );
-          setTimeout(() => {
-            location.reload();
-          }, 1000);
-        },
-      });
-  }
+	ionViewDidEnter() {
+		localStorage.removeItem('userData');
+		this.storeService.userData.set([]);
+		this.crudService
+			.checkMaintenance()
+			.pipe(
+				map((res) => {
+					return res[0].VALUE === '1' ? true : false;
+				})
+			)
+			.subscribe({
+				next: (res) => {
+					this.maintenance = res;
+				},
+				error: () => {
+					this.alertMessage = 'Ocurrió un error inesperado, reintentando';
+					this.errorToast.present();
+					console.error('Ocurrió un error al intentar verificar si la aplicación está en mantenimiento');
+					setTimeout(() => {
+						location.reload();
+					}, 1000);
+				},
+			});
+	}
 
-  updateTime() {
-    this.interval = setInterval(() => {
-      // const campDate = dayjs('2024-06-08 04:00');
-      // const dateNow = dayjs();
-      // this.countDown = dayjs(campDate.diff(dateNow))
-      this.countDown = this.countDown.add(5, 'hour');
-    }, 1000);
-  }
+	login(): void {
+		this.saveAdminData = [];
+		this.showLoadingText = true;
+		this.enableButton = false;
+		this.errorToast.dismiss();
+		if (this.maintenance !== null) {
+			if (this.maintenance) {
+				this.alertMessage = 'Estamos en mantenimiento, intente nuevamente más tarde';
+				this.errorToast.present();
+				this.showLoadingText = false;
+				this.enableButton = true;
+			} else {
+				if (this.checkError()) {
+					this.crudService.searchDocument(this.document.value || '', this.type.value || '').subscribe({
+						next: (res) => {
+							if (res.length === 0) {
+								this.showLoadingText = false;
+								this.enableButton = true;
+								this.alertMessage = 'Documento incorrecto';
+								this.errorToast.present();
+							} else {
+								if (res[0].ADMIN > 0) {
+									this.enableAdmin = true;
+									this.enableButton = true;
+									this.showLoadingText = false;
+									this.saveAdminData = res;
+								} else {
+									this.enableAdmin = false;
+									this.storeService.userData.set(res);
+									localStorage.setItem('userData', JSON.stringify(res));
+									this.router.navigateRoot(['/progreso']);
+								}
+							}
+						},
+						error: (err) => {
+							if (err.status === 409) {
+								this.alertMessage = 'Número de documento incorrecto';
+							} else {
+								this.alertMessage = 'Ocurrió un error, intente nuevamente';
+								console.error('Ocurrió un error: ', err);
+							}
+							this.errorToast.present();
+							this.showLoadingText = false;
+							this.enableButton = true;
+						},
+					});
+				}
+			}
+		} else {
+			setTimeout(() => {
+				this.login();
+			}, 100);
+		}
+	}
 
-  login(): void {
-    this.saveAdminData = [];
-    this.showLoadingText = true;
-    this.enableButton = false;
-    this.errorToast.dismiss();
-    if (this.maintenance !== null) {
-      if (this.maintenance) {
-        this.alertMessage =
-          'Estamos en mantenimiento, intente nuevamente más tarde';
-        this.errorToast.present();
-        this.showLoadingText = false;
-        this.enableButton = true;
-      } else {
-        if (this.checkError()) {
-          this.crudService
-            .searchDocument(this.document.value || '', this.type.value || '')
-            .subscribe({
-              next: (res) => {
-                if (res.length === 0) {
-                  this.showLoadingText = false;
-                  this.enableButton = true;
-                  this.alertMessage = 'Documento incorrecto';
-                  this.errorToast.present();
-                } else {
-                  if (res[0].ADMIN > 0) {
-                    this.enableAdmin = true;
-                    this.enableButton = true;
-                    this.showLoadingText = false;
-                    this.saveAdminData = res;
-                  } else {
-                    this.enableAdmin = false;
-                    this.storeService.userData.set(res);
-                    localStorage.setItem('userData', JSON.stringify(res));
-                    this.router.navigateRoot(['/progreso']);
-                  }
-                }
-              },
-              error: (err) => {
-                if (err.status === 409) {
-                  this.alertMessage = 'Número de documento incorrecto';
-                } else {
-                  this.alertMessage = 'Ocurrió un error, intente nuevamente';
-                  console.error('Ocurrió un error: ', err);
-                }
-                this.errorToast.present();
-                this.showLoadingText = false;
-                this.enableButton = true;
-              },
-            });
-        }
-      }
-    } else {
-      setTimeout(() => {
-        this.login();
-      }, 100);
-    }
-  }
+	loginAdmin() {
+		this.showLoadingText = true;
+		this.enableButton = false;
+		const requestBody: any = {
+			document: this.document.value,
+			type: this.type.value,
+			password: this.password.value,
+		};
+		this.crudService.loginAdmin(requestBody).subscribe({
+			next: (res) => {
+				this.storeService.userData.set(res);
+				localStorage.setItem('userData', JSON.stringify(this.saveAdminData));
+				this.router.navigateRoot(['/progreso']);
+				this.showLoadingText = false;
+				this.enableButton = true;
+			},
+			error: () => {
+				this.alertMessage = 'Contraseña incorrecta';
+				this.errorToast.present();
+				this.showLoadingText = false;
+				this.enableButton = true;
+			},
+		});
+	}
 
-  loginAdmin() {
-    this.showLoadingText = true;
-    this.enableButton = false;
-    const requestBody: any = {
-      document: this.document.value,
-      type: this.type.value,
-      password: this.password.value,
-    };
-    this.crudService.loginAdmin(requestBody).subscribe({
-      next: (res) => {
-        this.storeService.userData.set(res);
-        localStorage.setItem('userData', JSON.stringify(this.saveAdminData));
-        this.router.navigateRoot(['/progreso']);
-        this.showLoadingText = false;
-        this.enableButton = true;
-      },
-      error: () => {
-        this.alertMessage = 'Contraseña incorrecta';
-        this.errorToast.present();
-        this.showLoadingText = false;
-        this.enableButton = true;
-      },
-    });
-  }
-
-  checkError(): boolean {
-    if (this.document.invalid) {
-      this.alertMessage = 'Introduza su número de documento';
-      this.errorToast.present();
-      this.showLoadingText = false;
-      this.enableButton = true;
-      return false;
-    } else if (this.type.invalid) {
-      this.alertMessage = 'Seleccione su tipo de documento';
-      this.errorToast.present();
-      this.showLoadingText = false;
-      this.enableButton = true;
-      return false;
-    } else {
-      return true;
-    }
-  }
+	checkError(): boolean {
+		if (this.document.invalid) {
+			this.alertMessage = 'Introduza su número de documento';
+			this.errorToast.present();
+			this.showLoadingText = false;
+			this.enableButton = true;
+			return false;
+		} else if (this.type.invalid) {
+			this.alertMessage = 'Seleccione su tipo de documento';
+			this.errorToast.present();
+			this.showLoadingText = false;
+			this.enableButton = true;
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
