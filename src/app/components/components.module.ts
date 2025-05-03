f83e2448e5b14c1a029bad5a8e291a559dc4177c
Alejandro <luisrios.lar@gmail.com>
2025-05-03 00:17:47 -0500

    style: progress bar style

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertsComponent } from './alerts/alerts.component';
 import { PbarComponent } from './pbar/pbar.component';
-
-
+import { PercentPipe } from '../pipes/percent.pipe';
 
 @NgModule({
-  declarations: [AlertsComponent, PbarComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [AlertsComponent, PbarComponent]
+	declarations: [AlertsComponent, PbarComponent],
+	imports: [CommonModule, PercentPipe],
+	exports: [AlertsComponent, PbarComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
--- src/app/components/pbar/pbar.component.ts
+++ src/app/components/pbar/pbar.component.ts
@@ -1,18 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'pbar',
-    templateUrl: './pbar.component.html',
-    styleUrls: ['./pbar.component.scss'],
-    standalone: false
+	selector: 'pbar',
+	templateUrl: './pbar.component.html',
+	styleUrls: ['./pbar.component.scss'],
+	standalone: false,
 })
-export class PbarComponent  implements OnInit {
+export class PbarComponent implements OnInit {
+	@Input('value') value: number = 0;
+	@Input('confirmed') confirmed: number = 0;
 
-  @Input("value") value: number = 0;
-  @Input("confirmed") confirmed: number = 0;
-  
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
+	progressLabel(value: number) {
+		if (value <= 0) return '¡Tu puedes!';
+		if (value > 0 && value < 25) return '¡Buen comienzo!';
+		if (value >= 25 && value < 75) return 'Sigue así';
+		if (value >= 75 && value < 100) return 'Falta poco';
+		if (value >= 100) return '🎉';
+		else return 'Progreso';
+	}
 }
